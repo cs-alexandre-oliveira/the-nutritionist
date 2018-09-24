03a9cee4c28e133865434589e458e3ffcf7aaefa
Alexandre DEV <calexandredev@gmail.com>
2018-09-24 07:20:05 -0300

    adicionado validação para calcular o imc

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,32 @@
 let title = document.querySelector('.title')
     title.textContent = 'The Nutritionist'
+
+let patient = document.querySelector('#first-patient')
+let infoPeso = patient.querySelector('.info-peso')
+let weight = infoPeso.textContent 
+
+let infoAltura = patient.querySelector('.info-altura')
+let height = infoAltura.textContent
+
+
+var insertImc = patient.querySelector('.info-imc')
+
+let valueHeight = true
+let valueWeight = true
+
+if(weight <= 0 || weight >= 1000) {
+  valueWeight = false
+  insertImc.textContent = 'Peso Invalido'
+  console.log(imc)
+}
+
+if(height <= 0 || height >= 1000) {
+  console.log('Altura Invalida!')
+  valueHeight = false
+  insertImc.textContent = 'Altura Invalida'
+}
+
+if(weight && height) {
+  let imc = weight / (height * height)
+  insertImc.textContent = imc
+}
