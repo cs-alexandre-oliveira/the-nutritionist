9064169e5bb7430b60bf23d5a29adf205c0c1225
Alexandre DEV <calexandredev@gmail.com>
2018-09-30 21:38:35 -0300

    adicionado loop for para calcular o imc da lista

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,39 @@
 let title = document.querySelector('.title')
     title.textContent = 'The Nutritionist'
 
-let patient = document.querySelector('#first-patient')
-let infoPeso = patient.querySelector('.info-peso')
-let weight = infoPeso.textContent 
+let patients = document.querySelectorAll('.paciente')
+console.log('==>>>>', patients)
 
-let infoAltura = patient.querySelector('.info-altura')
-let height = infoAltura.textContent
+for(let i = 0; i < patients.length; i++) {
+  let patient = patients[i]
 
-
-var insertImc = patient.querySelector('.info-imc')
-
-let valueHeight = true
-let valueWeight = true
-
-if(weight <= 0 || weight >= 1000) {
-  valueWeight = false
-  insertImc.textContent = 'Peso Invalido'
-  console.log(imc)
+  let infoPeso = patient.querySelector('.info-peso')
+  let weight = infoPeso.textContent 
+  
+  let infoAltura = patient.querySelector('.info-altura')
+  let height = infoAltura.textContent
+  
+  
+  var insertImc = patient.querySelector('.info-imc')
+  
+  let valueHeight = true
+  let valueWeight = true
+  
+  if(weight <= 0 || weight >= 1000) {
+    valueWeight = false
+    insertImc.textContent = 'Peso Invalido'
+    console.log(imc)
+  }
+  
+  if(height <= 0 || height >= 1000) {
+    valueHeight = false
+    insertImc.textContent = 'Altura Invalida'
+    console.log(imc)
+  }
+  
+  if(weight && height) {
+    let imc = weight / (height * height)
+    insertImc.textContent = imc.toFixed(2)
+  }
 }
 
-if(height <= 0 || height >= 1000) {
-  valueHeight = false
-  insertImc.textContent = 'Altura Invalida'
-  console.log(imc)
-}
-
-if(weight && height) {
-  let imc = weight / (height * height)
-  insertImc.textContent = imc
-}
