be8f31aca074831ca06be238cd1013464ec99565
Alexandre DEV <calexandredev@gmail.com>
2018-10-01 19:10:55 -0300

    adicionado cor na linha quando estiver errado

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,38 @@
-let title = document.querySelector('.title')
+var title = document.querySelector('.title')
     title.textContent = 'The Nutritionist'
 
-let patients = document.querySelectorAll('.paciente')
+var patients = document.querySelectorAll('.paciente')
 console.log('==>>>>', patients)
 
-for(let i = 0; i < patients.length; i++) {
-  let patient = patients[i]
+for(var i = 0; i < patients.length; i++) {
+  var patient = patients[i]
 
-  let infoPeso = patient.querySelector('.info-peso')
-  let weight = infoPeso.textContent 
+  var infoPeso = patient.querySelector('.info-peso')
+  var weight = infoPeso.textContent 
   
-  let infoAltura = patient.querySelector('.info-altura')
-  let height = infoAltura.textContent
+  var infoAltura = patient.querySelector('.info-altura')
+  var height = infoAltura.textContent
   
   
   var insertImc = patient.querySelector('.info-imc')
   
-  let valueHeight = true
-  let valueWeight = true
+  var valueHeight = true
+  var valueWeight = true
   
   if(weight <= 0 || weight >= 1000) {
     valueWeight = false
     insertImc.textContent = 'Peso Invalido'
-    console.log(imc)
+    patient.classList.add('patient-alert')
   }
   
   if(height <= 0 || height >= 1000) {
     valueHeight = false
     insertImc.textContent = 'Altura Invalida'
-    console.log(imc)
+    patient.classList.add('patient-alert')
   }
   
   if(weight && height) {
-    let imc = weight / (height * height)
+    var imc = weight / (height * height)
     insertImc.textContent = imc.toFixed(2)
   }
 }
